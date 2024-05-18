cb01d63a8579e61b0a91521dbe3f31387bb5f175
massmoussi <assermouh.abdelhak2019@gmail.com>
2024-05-19 00:14:21 +0100

    testing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-import json
-from models.base_model import BaseModel
+# models/engine/file_storage.py
 
+import json
+from os import path
 
 class FileStorage:
     __file_path = "file.json"
@@ -12,20 +13,21 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        key = f"{obj.__class__.__name__}.{obj.id}"
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
         with open(self.__file_path, 'w') as f:
-            json_objects = {k: v.__dict__ for k, v in self.__objects.items()}
-            json.dump(json_objects, f)
+            json.dump(serialized_objects, f)
 
     def reload(self):
-        try:
+        if path.exists(self.__file_path):
             with open(self.__file_path, 'r') as f:
-                json_objects = json.load(f)
-                for key, value in json_objects.items():
+                serialized_objects = json.load(f)
+                for key, obj_dict in serialized_objects.items():
                     class_name, obj_id = key.split('.')
-                    self.__objects[key] = BaseModel(**value)
-        except FileNotFoundError:
-            pass
+                    obj_class = globals()[class_name]
+                    self.__objects[key] = obj_class(**obj_dict)
