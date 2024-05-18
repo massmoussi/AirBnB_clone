032e11a0ba7aaa82944ee98716cbba514d2fb1e2
massmoussi <assermouh.abdelhak2019@gmail.com>
2024-05-19 00:06:06 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/ env
+#!/usr/bin/python3
 
 import uuid
 from datetime import datetime
@@ -7,20 +7,37 @@ import models
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+
+        dfrmat = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, dfrmat))
+                elif key == '__class__':
+                    continue
+                    # pass
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            # models.storage.new(self)
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
+
         return obj_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,44 +1,31 @@
 #!/usr/bin/python3
 
-from models.base_model import BaseModel
-from models.user import User
 import json
+from models.base_model import BaseModel
 
 
 class FileStorage:
-    """Class to handle serialization and deserialization of objects"""
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
-        """Set in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
-        new_dict = {}
-        for key, value in FileStorage.__objects.items():
-            new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(new_dict, f)
+        with open(self.__file_path, 'w') as f:
+            json_objects = {k: v.__dict__ for k, v in self.__objects.items()}
+            json.dump(json_objects, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file_path, 'r') as f:
-                data = json.load(f)
-                for key, value in data.items():
-                    class_name = value["__class__"]
-                    if class_name == "BaseModel":
-                        obj = BaseModel(**value)
-                    elif class_name == "User":
-                        obj = User(**value)
-                    FileStorage.__objects[key] = obj
+            with open(self.__file_path, 'r') as f:
+                json_objects = json.load(f)
+                for key, value in json_objects.items():
+                    class_name, obj_id = key.split('.')
+                    self.__objects[key] = BaseModel(**value)
         except FileNotFoundError:
             pass
