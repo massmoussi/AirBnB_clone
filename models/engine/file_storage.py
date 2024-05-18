d9727e5f1e5e1589932b5fc6ae47d26aff85b918
massmoussi <assermouh.abdelhak2019@gmail.com>
2024-05-18 23:40:15 +0100

    testing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python3
 
-import json
 from models.base_model import BaseModel
+from models.user import User
+import json
 
 
 class FileStorage:
+    """Class to handle serialization and deserialization of objects"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        return self.__objects
+        """Return the dictionary __objects"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        """Set in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        with open(self.__file_path, 'w') as f:
-            json_objects = {k: v.__dict__ for k, v in self.__objects.items()}
-            json.dump(json_objects, f)
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(new_dict, f)
 
     def reload(self):
+        """Deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as f:
-                json_objects = json.load(f)
-                for key, value in json_objects.items():
-                    class_name, obj_id = key.split('.')
-                    self.__objects[key] = BaseModel(**value)
+            with open(FileStorage.__file_path, 'r') as f:
+                data = json.load(f)
+                for key, value in data.items():
+                    class_name = value["__class__"]
+                    if class_name == "BaseModel":
+                        obj = BaseModel(**value)
+                    elif class_name == "User":
+                        obj = User(**value)
+                    FileStorage.__objects[key] = obj
         except FileNotFoundError:
             pass
