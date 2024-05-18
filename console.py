7e688a38f1aa481fbff909ab930af039e1579a80
killua <killua@killua2.0>
2024-05-18 18:42:50 +0100

    fix it i think

--- console.py
+++ console.py
@@ -1,25 +1,55 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Console module for the HBNB command interpreter.
+"""
 
 import cmd
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
+    """Command interpreter class."""
 
-    prompt = '(hbnb)'
+    prompt = "(hbnb) "
 
-    def do_quit(self, line):
+    def emptyline(self):
+        """Override emptyline method to do nothing."""
+        pass
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on EOF."""
+        print()
         return True
 
     def help_quit(self):
+        """Print help message for the quit command."""
         print("Quit command to exit the program")
 
-    def do_EOF(self, line):
-        return True
-
     def help_EOF(self):
-        print("EOF command to EOF the program")
+        """Print help message for EOF."""
+        print("Exit the program on EOF")
+
+
+def run_interactive():
+    """Run the command interpreter in interactive mode."""
+    HBNBCommand().cmdloop()
+
+
+def run_non_interactive():
+    """Run the command interpreter in non-interactive mode."""
+    for line in sys.stdin:
+        cmd = line.strip()
+        if cmd:
+            print("(hbnb)")
+            HBNBCommand().onecmd(cmd)
 
 
 if __name__ == '__main__':
-
-    HBNBCommand().cmdloop()
+    if sys.stdin.isatty():
+        run_interactive()
+    else:
+        run_non_interactive()
