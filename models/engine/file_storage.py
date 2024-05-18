dfcb58fcce35cca7630a27e48c3e37942f52f789
massmoussi <assermouh.abdelhak2019@gmail.com>
2024-05-19 00:52:34 +0100

    fix the fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 # models/engine/file_storage.py
 
 import json
+from os import path
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -12,41 +13,29 @@ from models.amenity import Amenity
 from models.place import Place
 
 
-class FileStorage():
-    """ file storage system """
-
+class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ retrieve all objects """
         return self.__objects
 
     def new(self, obj):
-        """create new insance
-
-        Args:
-            obj (BaseModel): base model instance
-        """
-        if obj:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            self.__objects[key] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """ save new instance """
-        s_obj = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, "w", encoding="UTF-8") as f:
-            json.dump(s_obj, f)
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(serialized_objects, f)
 
     def reload(self):
-        """ reload existed instances """
-        try:
-            with open(self.__file_path, "r", encoding="UTF-8") as f:
-                data = json.load(f)
-            for key, value in data.items():
-                class_name, obj_id = key.split('.')
-                class_obj = globals()[class_name]
-                instance = class_obj(**value)
-                self.__objects[key] = instance
-        except FileNotFoundError:
-            pass
+        if path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                serialized_objects = json.load(f)
+                for key, obj_dict in serialized_objects.items():
+                    class_name, obj_id = key.split('.')
+                    obj_class = globals()[class_name]
+                    self.__objects[key] = obj_class(**obj_dict)
